883711f7c83aab640eba9ce5ac49617ca33c3fa2
Maja Sørensen <majabirchsorensen@gmail.com>
2022-03-31 13:32:11 +0200

    extend removed + layer base started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        beige: "#ECE8DD",
-        beigeHover: "hsl(44, 28%, 92%)",
-        brown: "#DFD5CB",
-        grey: "#3A3A3A",
-        greyHover: "hsl(0, 0%, 30%)",
-        darkGrey: "hsl(0, 0%, 20%)",
-        black: "#000000",
-      },
+    extend: {},
+    colors: {
+      beige: "#ECE8DD",
+      beigeHover: "hsl(44, 28%, 92%)",
+      brown: "#DFD5CB",
+      grey: "#3A3A3A",
+      greyHover: "hsl(0, 0%, 30%)",
+      darkGrey: "hsl(0, 0%, 20%)",
+      black: "#000000",
     },
     fontFamily: {
       'imbue': "'Imbue', serif",
